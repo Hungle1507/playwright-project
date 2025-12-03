fbec23da4c6827de61050183612512fd2eb26880
Hung Le Minh <hung.m.le@opswat.com>
2025-12-03 23:18:08 +0700

    test login

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/loginPage.js'
 
-test('open playwright website', async ({ page }) => {
-  await page.goto('https://playwright.dev');
-  await expect(page).toHaveTitle(/Playwright/);
+
+// test('open playwright website', async ({ page }) => {
+//   await page.goto('https://playwright.dev');
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+test ('try login haroku web', async ({page}) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.gotoPage();
+  await loginPage.login('tomsmith', 'SuperSecretPassword!');
+  await expect(loginPage.message).toContainText('You logged into a secure area!');
 });
